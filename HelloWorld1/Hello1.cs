518b9020a350f20b6485658cb0e436aaa955185e
asmith <asmith@lrwonline.com>
2014-12-16 16:23:05 -0800

    Upgrade to vs2013

--- HelloWorld1/Hello1.cs
+++ HelloWorld1/Hello1.cs
@@ -1,11 +1,15 @@
 ﻿//Copyright (C) Microsoft Corporation.  All rights reserved.
 
 // Hello1.cs
+
+using System;
+
 public class Hello1
 {
    public static void Main()
    {
       System.Console.WriteLine("Hello, World!");
+       Console.ReadKey();
    }
 }
 
